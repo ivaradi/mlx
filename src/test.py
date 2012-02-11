6142ebb86de42ceba7e5a1fe24cc9447df900826
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2012-02-11 20:55:26 +0100

    Implemented a simple GUI for debugging.

--- src/test.py
+++ src/test.py
@@ -16,7 +16,7 @@ def main():
     with open(sys.argv[1], "wt") as output:
         fl = flight.Flight(logger.Logger(output = output))
         fl.cruiseAltitude = 18000
-        fl.aircraftType = const.AIRCRAFT_B736
+        fl.aircraftType = const.AIRCRAFT_DH8D
         fl.aircraft = acft.Aircraft.create(fl)
         #fl._stage = const.STAGE_LANDING
         simulator = fs.createSimulator(const.SIM_MSFS9, fs.ConnectionListener(),
