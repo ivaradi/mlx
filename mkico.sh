a1ea86bf714c1ee5747e804c2cb80e8a148b074d
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2012-02-26 13:34:42 +0100

    Fixed the icons

---
+++ mkico.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Save each icon in different sizes: 128x128, 96x96, 64x64, 48x48, 32x32, 16x16
+png2ico logo.ico logo128.png logo96.png logo64.png logo48.png logo32.png logo16.png
+png2ico logo_uninst.ico logo_uninst128.png logo_uninst96.png logo_uninst64.png logo_uninst48.png logo_uninst32.png logo_uninst16.png
