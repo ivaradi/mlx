e4a60e57e3a95b5a4f266692c0dc86c645bf0b0b
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2014-12-06 15:02:25 +0100

    Setting LD_LIBRARY_PATH to a more compatible OpenAL library

--- runmlx.sh
+++ runmlx.sh
@@ -5,4 +5,7 @@ scriptdir=`dirname $0`
 PYTHONPATH="${scriptdir}/src:${scriptdir}:${PYTHONPATH}"
 export PYTHONPATH
 
+LD_LIBRARY_PATH="${HOME}/local/opt/OpenAL/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH
+
 exec python -m runmlx "$@"
