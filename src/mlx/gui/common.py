c98a726cad771b2df9a962b1222e0c77d7a615c7
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2015-12-11 19:38:06 +0100

    Updated the manual

--- src/mlx/gui/common.py
+++ src/mlx/gui/common.py
@@ -288,7 +288,7 @@ gobject.signal_new("integer-changed", IntegerEntry, gobject.SIGNAL_RUN_FIRST,
 
 PROGRAM_NAME = "MAVA Logger X"
 
-WINDOW_TITLE_BASE = PROGRAM_NAME + " " + _const.VERSION
+WINDOW_TITLE_BASE = PROGRAM_NAME
 if secondaryInstallation:
     WINDOW_TITLE_BASE += " (" + xstr("secondary") + ")"
 
