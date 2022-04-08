c280948a1dd65cbfab2b1f2bf884a77fbcdbb793
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2022-04-08 20:06:02 +0200

    The original MAVA website URL is used (re #357)

--- src/mlx/common.py
+++ src/mlx/common.py
@@ -8,7 +8,7 @@ import os
 #
 #---------------------------------------------------------------------------------------
 
-MAVA_BASE_URL = os.environ.get("MAVA_BASE_URL", "https://new.virtualairlines.hu")
+MAVA_BASE_URL = os.environ.get("MAVA_BASE_URL", "https://virtualairlines.hu")
 
 #-------------------------------------------------------------------------------
 
