f7b42f48f6e598012daa344d7c723919e54acdb5
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2022-04-10 15:48:20 +0200

    CoG is taken from a different dataref for Zibo/LevelUp 737NG

--- src/mlx/xplane.py
+++ src/mlx/xplane.py
@@ -1734,7 +1734,7 @@ class ZiboB737NGModel(B737Model):
                                         (TYPE_FLOAT_ARRAY, 2))
         self._cgIndex = len(data)
         self._addDatarefWithIndexMember(data,
-                                        "laminar/B738/fms/calc_to_cg",
+                                        "laminar/B738/tab/cg_pos",
                                         TYPE_FLOAT)
         self._wingHeatIndex = len(data)
         self._addDatarefWithIndexMember(data,
