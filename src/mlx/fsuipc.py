c3011a89ab8234bf1e87def8ab65f4378a6a6adf
István Váradi <ivaradi+gitlab@varadiistvan.hu>
2023-06-09 17:07:00 +0200

    FSUIPC offset 0x0590 is handled as an integer on MSFS 2020 (re #366)

--- src/mlx/fsuipc.py
+++ src/mlx/fsuipc.py
@@ -1233,7 +1233,7 @@ class AircraftModel(object):
                       ("vs", 0x02c8, "d"),
                       ("radioAltitude", 0x31e4, "d"),
                       ("altitude", 0x0570, "l"),
-                      ("msfs2020Altitude", 0x0590, "F"),
+                      ("msfs2020Altitude", 0x0590, "d"),
                       ("gLoad", 0x11ba, "h"),
                       ("flapsControl", 0x0bdc, "d"),
                       ("flapsLeft", 0x0be0, "d"),
